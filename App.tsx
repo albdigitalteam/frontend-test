da533e7eb009504218804fb00c47ce3f8302d592
C1310213 Bruno Dias Donnici <c1310213@interno.bb.com.br>
2021-03-30 08:36:13 -0300

    feat: Initial architecture

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+    return (
+        <View 
+            style={{
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center',
+                alignContent: 'center'
+            }
+            }
+        >
+            <Text>Olá mundo</Text>
+        </View>
+    )
+}
+
+export default App
