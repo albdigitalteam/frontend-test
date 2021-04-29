76394a5c4599913121a838f97ed89982fb301d92
Gilmar Vogel <desenv2@idata.com.br>
2021-04-29 12:18:57 -0300

    rename Loader folder

--- src/components/Loaders/Skeleton/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Skeleton from './index';
-
-test('render Skeleton', () => {
-  render(<Skeleton />)
-  expect(screen.getByTestId('skeleton')).toBeInTheDocument();
-});
-
-
