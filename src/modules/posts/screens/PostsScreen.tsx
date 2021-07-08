b978247e2118bb17ab546fc6179094e11246903f
Manoel Ramos <manoel-neto-@hotmail.com>
2021-07-08 17:03:38 -0300

    add loading into posts screens

--- src/modules/posts/screens/PostsScreen.tsx
+++ src/modules/posts/screens/PostsScreen.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, ActivityIndicator } from 'react-native';
 import { useAppDispatch, useAppSelector } from '@hooks/redux';
 import { StyledView } from '@components/components';
-import { selectGetPosts } from '@stores/post/post.store';
+import { selectGetPosts, selectPostsIsLoading } from '@stores/post/post.store';
 import { getPostsAction } from '@stores/post/post.types';
 import { getCommentsAction } from '@stores/comment/comment.types';
 import { getUserAction } from '@stores/user/user.types';
@@ -11,12 +11,21 @@ import PostItem from '../components/PostItem';
 export default function PostsScreen(): JSX.Element {
   const dispatch = useAppDispatch();
   const posts = useAppSelector(selectGetPosts);
+  const isLoading = useAppSelector(selectPostsIsLoading);
 
   useEffect(() => {
     dispatch(getCommentsAction());
     dispatch(getUserAction());
     dispatch(getPostsAction());
   }, [dispatch]);
+
+  if (isLoading) {
+    return (
+      <StyledView flex={1} alignItems="center" justifyContent="center">
+        <ActivityIndicator size="large" />
+      </StyledView>
+    );
+  }
 
   return (
     <StyledView flex={1}>
