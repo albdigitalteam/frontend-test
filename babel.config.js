84f15c77302578fc4d7cd49d15ddd36a2fa0f7ae
Rafael Sosnowski <rafael.sosnowski@gmail.com>
2021-07-13 15:02:08 -0300

    config eslint, prettier

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
 module.exports = function(api) {
   api.cache(true);
+  
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'babel-plugin-root-import',
+        {
+          paths: [
+            {
+              rootPathSuffix: './src',
+              rootPathPrefix: '~/',
+            },
+          ],
+        },
+      ],
+    ],
   };
 };
---
+++ src/routes/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Home } from '~/screens/Home';
+
+const Routes: React.FC = () => {
+  return <Home />;
+};
+
+export { Routes };
