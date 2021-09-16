48c318f7cbbad9665eb025872a8ae71fb8ef4181
gabrielbsandim <gabriel.bastos@safiraenergia.com.br>
2021-09-16 01:52:22 -0300

    # Feat: add error form component

--- src/app/shared/shared-module/shared.module.ts
+++ src/app/shared/shared-module/shared.module.ts
@@ -2,13 +2,17 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { ErrorContainerComponent } from 'src/app/components/error-container/error-container.component';
+import { ErrorMessageComponent } from 'src/app/components/error-message/error-message.component';
 import { PostsComponent } from 'src/app/components/posts/posts.component';
 import { UserDataComponent } from 'src/app/pages/profile/components/user-data/user-data.component';
 
 @NgModule({
   declarations: [
     UserDataComponent,
-    PostsComponent
+    PostsComponent,
+    ErrorContainerComponent,
+    ErrorMessageComponent
   ],
   imports: [
     CommonModule,
@@ -18,7 +22,9 @@ import { UserDataComponent } from 'src/app/pages/profile/components/user-data/us
   ],
   exports: [
     UserDataComponent,
-    PostsComponent
+    PostsComponent,
+    ErrorContainerComponent,
+    ErrorMessageComponent
   ],
 })
 export class SharedModule { }
