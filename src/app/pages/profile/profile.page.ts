9a533d292d71d6ba8af11adf2ad7486d352fbf83
gabrielbsandim <gabriel.bastos@safiraenergia.com.br>
2021-09-15 23:56:49 -0300

    # Feat: add user not found and other users profile

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { IUser } from 'src/app/models/user.model';
 import { UserService } from 'src/app/services/user.service';
+import { IToastData, ToastCreate } from 'src/app/utils/toastCreate.util';
 
 @Component({
   selector: 'app-profile',
@@ -10,9 +12,40 @@ import { UserService } from 'src/app/services/user.service';
 export class ProfilePage implements OnInit {
   public user: IUser = null;
 
-  constructor(private userService: UserService) { }
+  private toastCreate = new ToastCreate();
+
+
+  constructor(private userService: UserService, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    const userId = this.activatedRoute.snapshot.paramMap.get('userId');
+
+    if (!userId) {
+      return this.getCurrentUser();
+    }
+
+    // o melhor nesse caso, seria ter uma rota para buscar apenas 1 usuário
+    this.userService.fetchUsers().subscribe(users => {
+      const user = users.find(el => el.id === Number(userId));
+
+      if (!user) {
+        return;
+      }
+
+      this.user = user;
+    }, async error => {
+      console.log(error);
+
+      const toastData: IToastData = {
+        message: error,
+        color: 'danger',
+      };
+
+      await this.toastCreate.create(toastData);
+    });
+  }
+
+  private getCurrentUser() {
     this.user = this.userService.getCurrentUser();
   }
 
