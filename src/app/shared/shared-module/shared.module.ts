6dc9dc754237081b89cf0a48cec956ce87e320ff
gabrielbsandim <gabriel.bastos@safiraenergia.com.br>
2021-09-16 21:32:52 -0300

    # Refact: move posts component

--- src/app/shared/shared-module/shared.module.ts
+++ src/app/shared/shared-module/shared.module.ts
@@ -4,7 +4,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ErrorContainerComponent } from 'src/app/components/error-container/error-container.component';
 import { ErrorMessageComponent } from 'src/app/components/error-message/error-message.component';
-import { PostsComponent } from 'src/app/components/posts/posts.component';
+import { PostsComponent } from 'src/app/pages/feed/components/posts/posts.component';
 import { UserDataComponent } from 'src/app/pages/profile/components/user-data/user-data.component';
 
 @NgModule({
