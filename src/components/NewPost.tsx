34538e5d1e7e4250f9c9a8d512645da36fffd822
Maurício Vieira <mauriciomvdr@hotmail.com>
2021-09-26 20:42:00 -0300

    feat: finishes new post component

--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -1,52 +1,57 @@
-import React from 'react';
-import { View, Dimensions } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import React, { useState } from 'react';
 import FAIcon from 'react-native-vector-icons/FontAwesome';
+import styled, { useTheme } from 'styled-components/native';
 
-import styled from 'styled-components/native';
-
-const Hello: React.FC = () => {
+const NewPost: React.FC = () => {
+  const [title, setTitle] = useState<string>('');
+  const {
+    colors: { secondary },
+  } = useTheme();
   return (
     <StyledContainer>
-      <Icon name="attach-file" size={30} color="#900" />
-      <StyledTextInput placeholder='Digite aqui o seu texto'/>
-      <FAIcon name="send-o" size={30} color="#900" />
+      <StyledTitleContainer>
+        <StyledTextInput
+          placeholder="Digite aqui o titulo..."
+          value={title}
+          onChangeText={setTitle}
+        />
+        <Button>
+          <FAIcon name="send-o" size={25} color={secondary} />
+        </Button>
+      </StyledTitleContainer>
+      {title.trim().length > 0 && (
+        <StyledInput placeholder="Digite o seu texto aqui..." />
+      )}
     </StyledContainer>
   );
 };
 
 const StyledContainer = styled.View`
-  flex-direction: row;
-  align-items: center;
-  background-color: #fefbf3;
+  background-color: ${({ theme: { colors } }) => colors.primary};
   box-shadow: 0px 3px 15px rgba(86, 86, 86, 0.15);
-  padding: 0 12px 12px;
   margin: 0 12px 16px;
+  padding: 10px 12px 10px;
   border-radius: 10px;
 `;
 
 const StyledTextInput = styled.TextInput`
   flex: 1;
+  font-size: 16px;
 `;
 
-const StyledText = styled.Text`
-  font-size: 20px;
-  margin-top: 12px;
-  margin-bottom: 12px;
+const StyledInput = styled.TextInput`
+  margin-top: 8px;
+  font-size: 16px;
 `;
 
-const StyledTitle = styled(StyledText)`
-  font-weight: bold;
-  text-align: center;
+const StyledTitleContainer = styled.View`
+  flex-direction: row;
 `;
-
 const Button = styled.TouchableOpacity`
   align-items: center;
   justify-content: center;
+  height: 35px;
+  width: 35px;
 `;
 
-const StyledButtonText = styled.Text`
-  color: #9d9d9d;
-`;
-
-export default Hello;
+export default NewPost;
