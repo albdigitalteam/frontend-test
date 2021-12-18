53d3b34a6c49a4d9229f233a08ab4ad8c79cc8b4
Gabriel Amorim <gabrielmorim98@gmail.com>
2021-12-17 21:06:31 -0300

    style: home pg refactored to add more functions

--- airLiquideConect/src/app/pages/home/home.page.ts
+++ airLiquideConect/src/app/pages/home/home.page.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { IComment } from 'src/app/models/comment.model';
+import { forkJoin } from 'rxjs';
 import { IPost } from 'src/app/models/post.model';
+import { IToastData } from 'src/app/models/toast.model';
 import { IUser } from 'src/app/models/user.model';
 import { CommentsService } from 'src/app/services/comments.service';
 import { ImagesService } from 'src/app/services/images.service';
 import { LocalStorageService } from 'src/app/services/local-storage.service';
 import { PostsService } from 'src/app/services/posts.service';
 import { UsersService } from 'src/app/services/users.service';
+import { CreateToast } from 'src/app/utils/createToast.util';
 import { PostAdapter } from 'src/app/utils/post.adapter';
 
 @Component({
@@ -26,35 +28,70 @@ export class HomePage implements OnInit {
     private readonly imagesService: ImagesService,
     private readonly router: Router,
     private readonly localStorage: LocalStorageService,
-    private readonly postAdapter: PostAdapter
+    private readonly postAdapter: PostAdapter,
+    private readonly createToast: CreateToast
   ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ionViewDidEnter() {
     this.loadData();
   }
 
   public loadData() {
     this.isLoading = true;
 
-    this.postsService.getPosts().subscribe((postsArray: IPost[]) => {
-      const imagesArray = this.imagesService.getImages();
-      this.usersService.getUsers().subscribe((usersArray: IUser[]) => {
-        this.commentsService
-          .getComments()
-          .subscribe((commentsArray: IComment[]) => {
-            this.posts = this.postAdapter.adaptInitialPost(
-              postsArray,
-              usersArray,
-              commentsArray,
-              imagesArray
-            );
-            console.log(this.posts);
-            this.localStorage.setPosts(this.posts);
-            this.localStorage.setComments(commentsArray);
-            this.isLoading = false;
-          });
-      });
-    });
+    if (this.posts) {
+      this.posts = this.localStorage.getPosts();
+      this.isLoading = false;
+      return;
+    }
+
+    forkJoin({
+      postsArray: this.postsService.getPosts(),
+      usersArray: this.usersService.getUsers(),
+      commentsArray: this.commentsService.getComments(),
+      imagesArray: this.imagesService.getImages(),
+    }).subscribe(
+      (data) => {
+        this.isLoading = false;
+        const { postsArray, usersArray, commentsArray, imagesArray } = data;
+
+        this.posts = this.postAdapter.adaptInitialPost(
+          postsArray,
+          usersArray,
+          commentsArray,
+          imagesArray
+        );
+        this.localStorage.setPosts(this.posts);
+        this.localStorage.setComments(commentsArray);
+        this.isLoading = false;
+      },
+      async (error) => {
+        this.isLoading = false;
+
+        const toastData: IToastData = {
+          message: error,
+          color: 'danger',
+        };
+
+        await this.createToast.create(toastData);
+      }
+    );
+  }
+
+  public toggleTitle(postId: number): void {
+    const postTitleIndex = this.posts.findIndex((post) => post.id === postId);
+
+    this.posts[postTitleIndex]['showTitle'] =
+      !this.posts[postTitleIndex]['showTitle'];
+  }
+
+  public toggleBoddy(postId: number): void {
+    const postBodyIndex = this.posts.findIndex((post) => post.id === postId);
+
+    this.posts[postBodyIndex]['showContent'] =
+      !this.posts[postBodyIndex]['showContent'];
   }
 
   public goToPostDetails(postId: number): void {
